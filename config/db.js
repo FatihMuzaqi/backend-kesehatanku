9cfc5dc6416948756e5715112cf7b9ae8e243892
FatihMuzaqi <fatih.muzaqi123@gmail.com>
2025-06-10 23:10:47 +0700

    finish

--- config/db.js
+++ config/db.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const db = new Sequelize(
-    process.env.DATABASE_URL,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
